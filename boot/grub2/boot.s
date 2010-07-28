b884c90d885329849d42a5cedf502f130a65e2a3
kfreezen@gmail.com <chester@UbuntuServer.(none)>
2010-07-28 11:13:20 -0700

    boot.s stub commit

---
+++ boot/grub2/boot.s
@@ -0,0 +1,18 @@
+; I Hate the poorly documented status of Grub2, anyway...
+
+MBOOT_MAGIC		equ		
+
+[section .multiboot]
+
+; STUB!
+
+
+[section .text]
+
+global start
+
+; STUB!
+
+start:
+	cli
+	hlt
