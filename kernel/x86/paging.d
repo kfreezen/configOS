a52779c4836575fb5e2b5fc22b5f67170e0e7459
public <public@public-desktop.(none)>
2010-07-29 21:42:43 -0700

    paging.d: unfinished!

---
+++ kernel/x86/paging.d
@@ -0,0 +1,15 @@
+module kernel.x86.paging;
+
+extern (C) uint end;
+
+uint page_aligned_end = (&end & 0xFFFFF000) + 0x1000;
+
+uint* pageDir = cast(uint*) page_aligned_end;
+uint* pageTable = pageDir + 0x1000;
+
+static void initPageDir() {
+	int i = 0;
+	for(i = 0; i < 1024; i++) {
+		pageDir[i] = 0 | 2;
+	}
+	
